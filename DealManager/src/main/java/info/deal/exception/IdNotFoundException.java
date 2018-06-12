166f5e8e796fd8d092910b7dfebd5a3d089cd3f3
MacAndrzej <maciejukandrzej@gmail.com>
2018-06-12 12:11:55 +0200

    Poprawiony IdNotFoundException

--- DealManager/src/main/java/info/deal/exception/IdNotFoundException.java
+++ DealManager/src/main/java/info/deal/exception/IdNotFoundException.java
@@ -11,9 +11,6 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "Order was not found")
 public class IdNotFoundException extends Exception {
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 
 	public IdNotFoundException() {
