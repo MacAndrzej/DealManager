4153e0d2af1ec007ed28d1e6740402427bca1366
MacAndrzej <maciejukandrzej@gmail.com>
2018-06-12 11:20:34 +0200

    1. Back to main branch

--- DealManager/src/main/java/info/deal/dto/DealEntityBuilder.java
+++ DealManager/src/main/java/info/deal/dto/DealEntityBuilder.java
@@ -4,7 +4,11 @@ import java.util.Date;
 
 import info.deal.entity.Deal;
 import info.deal.entity.Systems;
-
+/**
+ * 
+ * @Created by am on 12 cze 2018
+ *
+ */
 public interface DealEntityBuilder {
 	
 	Deal build();
--- DealManager/src/main/java/info/deal/dto/SystemEntityBuilder.java
+++ DealManager/src/main/java/info/deal/dto/SystemEntityBuilder.java
@@ -4,19 +4,19 @@ import java.util.List;
 
 import info.deal.entity.Deal;
 
+/**
+ * 
+ * @Created by am on 12 cze 2018
+ *
+ */
 public interface SystemEntityBuilder {
 
 	System build();
 
 	SystemEntityBuilderImpl id(final Long id);
-
 	SystemEntityBuilderImpl descriptionOfSystem(final String descriptionOfSystem);
-
 	SystemEntityBuilderImpl descriptionOfTechnology(final String descriptionOfTechnology);
-
 	SystemEntityBuilderImpl systemName(final String systemName);
-
 	SystemEntityBuilderImpl systemOwner(final String systemOwner);
-
 	SystemEntityBuilderImpl deals(final List<Deal> deals);
 }
