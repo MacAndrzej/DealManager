e9991e78ddd73c9e3cbd7c65fc4107df632d2ebc
MacAndrzej <maciejukandrzej@gmail.com>
2018-06-24 14:12:06 +0200

    Komentarze do testów

--- DealManager/src/test/java/entity/DealTest.java
+++ DealManager/src/test/java/entity/DealTest.java
@@ -27,73 +27,97 @@ public class DealTest {
 
 	@Test
 	public void testSetId() {
+		// given
 		Long idValue = 4L;
 
+		// when
 		deal.setId(idValue);
 
+		// then
 		assertEquals(idValue, deal.getId());
 	}
 
 	@Test
 	public void testSetOrderNumber() {
+		// given
 		String orderNumberValue = "1/2018";
 
+		// when
 		deal.setOrderNumber(orderNumberValue);
 
+		// then
 		assertEquals(orderNumberValue, deal.getOrderNumber());
 	}
 
 	@Test
 	public void testSetFromDate() {
+		// given
 		Date toDateValue = new Date();
 
+		// when
 		deal.setToDate(toDateValue);
 
+		// then
 		assertEquals(toDateValue, deal.getToDate());
 	}
 
 	@Test
 	public void testSetToDate() {
+		// given
 		Date fromDateValue = new Date();
 
+		// when
 		deal.setFromDate(fromDateValue);
 
+		// then
 		assertEquals(fromDateValue, deal.getFromDate());
 	}
 
 	@Test
 	public void testSetAmount() {
+		// given
 		String amountValue = "3000";
 
+		// when
 		deal.setAmount(amountValue);
 
+		// then
 		assertEquals(amountValue, deal.getAmount());
 	}
 
 	@Test
 	public void testSetAmountPeriod() {
+		// given
 		String amountPeriod = "MONTH";
 
+		// when
 		deal.setAmountPeriod(amountPeriod);
 
+		// then
 		assertEquals(amountPeriod, deal.getAmountPeriod());
 	}
 
 	@Test
 	public void testSetActive() {
+		// given
 		Integer activeValue = 1;
 
+		// when
 		deal.setActive(activeValue);
 
+		// then
 		assertEquals(activeValue, deal.getActive());
 	}
 
 	@Test
 	public void testSetSystems() {
+		// given
 		Systems systemsValue = new SystemEntityBuilderImpl().id(1L).build();
 
+		// when
 		deal.setSystems(systemsValue);
 
+		// then
 		assertEquals(systemsValue, deal.getSystems());
 	}
 
